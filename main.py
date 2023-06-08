502ec386d8bd8b764926aa4d7b489080c6f651b4
Aleksei Kudinov <reanim86@yandex.ru>
2023-06-08 16:45:15 +0300

    correct models.py

--- main.py
+++ main.py
@@ -14,27 +14,30 @@ async def get_people(id):
 
 async def get_number_person():
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as client:
-        response = await client.get(f'https://swapi.dev/api/people/')
+        response = await client.get('https://swapi.dev/api/people/')
         json_data = await response.json()
         return json_data['count']
 
 async def paste_to_db(people_list):
     async with Session() as session:
         for people in people_list:
-            new_people = SwapiPeople(
-                name=people['name'],
-                birth_year=people['birth_year'],
-                eye_color=people['eye_color'],
-                gender=people['gender'],
-                hair_color=people['hair_color'],
-                height=people['height'],
-                # homeworld=people['homeworld'],
-                # mass=people['mass'],
-                # skin_color=people['skin_color']
-            )
-            session.add(new_people)
-            await session.commit()
-    return
+            if 'detail' in people:
+                print(f'id {people} unknow')
+            else:
+                new_people = SwapiPeople(
+                    name=people['name'],
+                    birth_year=people['birth_year'],
+                    eye_color=people['eye_color'],
+                    gender=people['gender'],
+                    hair_color=people['hair_color'],
+                    height=people['height'],
+                    homeworld=people['homeworld'],
+                    mass=people['mass'],
+                    skin_color=people['skin_color']
+                )
+                session.add(new_people)
+                await session.commit()
+        return
 
 async def main():
 
@@ -42,8 +45,8 @@ async def main():
         await con.run_sync(Base.metadata.create_all)
 
     number_persons = await get_number_person()
-    # for i in chunked(range(1, (number_persons + 1)), 10):
-    for i in chunked(range(1, 2), 10):
+    for i in chunked(range(1, (number_persons + 1)), 10):
+    # for i in chunked(range(1, 2), 10):
         person_coros = []
         for id in i:
             person_coro = get_people(id)
